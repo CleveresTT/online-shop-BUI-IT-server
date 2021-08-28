08143450fda0280631fea250b5a0e10e882d5472
CleveresTT <sergey_semenov01@mail.ru>
2021-08-28 19:22:46 +0300

    1

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 require('dotenv').config()
 const sequelize=require('./db')
 const express = require('express')
-const models = require('./models/models')
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
 const errorHandlerMiddleware = require('./middlewares/ErrorHandlingMiddleware')
 const filePathMiddleware = require('./middlewares/filePathMiddleware')
 const path = require('path')
+const config = require('config')
 
-const PORT = process.env.PORT || 5000
+const PORT = config.get("port") || 5000
 
 const app = express()
 
