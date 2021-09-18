308437a886ade736ecad23ca9ea8281100ef7711
CleveresTT <sergey_semenov01@mail.ru>
2021-09-18 18:18:13 +0300

    r

--- db.js
+++ db.js
@@ -1,12 +1,41 @@
+const { Pool } = require('pg');
+
+const pool = new Pool();
+
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
+pool.on('connect', (err, client) => {
+  if (err) console.error(err);
+  console.log(client);
+  console.log('Successfully connected to postgres.');
+});
+
 const {Sequelize} = require('sequelize')
 
-module.exports = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-      dialect: 'postgres',
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-  }
-)
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    host: 'ec2-54-74-77-126.eu-west-1.compute.amazonaws.com',
+    port: 5432,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  });
+  
+module.exports = sequelize;
+
+// module.exports = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//       dialect: 'postgres',
+//       host: process.env.DB_HOST,
+//       port: process.env.DB_PORT,
+//   }
+// )
